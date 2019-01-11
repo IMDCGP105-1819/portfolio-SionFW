68af3d8bd7d438e9ffb97489d8a7ce9556954bff
SionFW <43640017+SionFW@users.noreply.github.com>
2019-01-11 08:59:46 +0000

    Update Boundaries.cs
    Comments added

--- Assets/Scripts/Boundaries.cs
+++ Assets/Scripts/Boundaries.cs
@@ -7,7 +7,8 @@ public class Boundaries : MonoBehaviour {
 	private float objectWidth;
 	private float objectHeight;
 
-	// Use this for initialization
+	// This uses the camera to dictate the area of play, limiting the player from going off screen, firstly it starts by getting the object width and height by dividing the bounds of the sprite by 2
+	//it then uses .Clamp with the variables, viewPos, screenbounds, objecthight and objectwidth to calculate the borders of the game.
 	void Start () {
 		screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
 		objectWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
